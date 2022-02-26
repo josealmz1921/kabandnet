a62dd496fc70478a447b696815a169dde2362d51
josealmz <joseharry1921@gmail.com>
2022-02-25 20:01:59 -0600

    Test

--- pagina/components/Beneficio.js
+++ pagina/components/Beneficio.js
@@ -6,7 +6,7 @@ const Beneficio = ({img,title}) => {
             <div className="p-2 sm:p-4">
                 <Image layout="responsive" src={img} width={200} height={200} />
             </div>
-            <p>Cambios en las ramas</p>
+            <p>Cambios en las ramas s</p>
             <p className="text-white text-lg sm:text-xl font-semibold text-center">{title ? title : ''}</p>
         </div>
      );
