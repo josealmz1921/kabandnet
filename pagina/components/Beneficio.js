299b850a9f0a9f3ade190e7867d6092954853dbe
josealmz <joseharry1921@gmail.com>
2022-02-25 18:33:37 -0600

    Edicion de linea

--- pagina/components/Beneficio.js
+++ pagina/components/Beneficio.js
@@ -6,6 +6,7 @@ const Beneficio = ({img,title}) => {
             <div className="p-2 sm:p-4">
                 <Image layout="responsive" src={img} width={200} height={200} />
             </div>
+            <p>Cambios en las ramas</p>
             <p className="text-white text-lg sm:text-xl font-semibold text-center">{title ? title : ''}</p>
         </div>
      );
